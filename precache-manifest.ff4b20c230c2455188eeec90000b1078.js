5197faab8bf1b9b8dbfa8d92b3828081f1936976
Roman <roman.svitelskyi@gmail.com>
2022-06-21 18:46:57 +0300

    Updates

---
+++ precache-manifest.ff4b20c230c2455188eeec90000b1078.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baaa636dc720f72bec0f62dc5e6cf1d8",
+    "url": "/millionaire/index.html"
+  },
+  {
+    "revision": "0b44f55aa64e87e96101",
+    "url": "/millionaire/static/css/main.d5caf168.chunk.css"
+  },
+  {
+    "revision": "83d1dbd77df451b98d4e",
+    "url": "/millionaire/static/js/2.f4592aa5.chunk.js"
+  },
+  {
+    "revision": "736f3ad4ee5990319c2a87119f7ebea6",
+    "url": "/millionaire/static/js/2.f4592aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b44f55aa64e87e96101",
+    "url": "/millionaire/static/js/main.d699a05e.chunk.js"
+  },
+  {
+    "revision": "078e70291101148e5f5b",
+    "url": "/millionaire/static/js/runtime-main.70d563fc.js"
+  }
+]);
